e08af766b49acc30f97a88a136e90de540a12305
upendrathatikonda <103295431+upendrathatikonda@users.noreply.github.com>
2023-07-02 09:52:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
